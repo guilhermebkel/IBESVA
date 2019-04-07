000c76cff74aa000e83f1a91a347d0ee80485544
Guilherme <guilhermebromonschenkel@gmail.com>
2019-04-06 22:38:18 -0300

    Sending all source code files to server

---
+++ blog/wp-config.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * The base configuration for WordPress
+ *
+ * The wp-config.php creation script uses this file during the
+ * installation. You don't have to use the web site, you can
+ * copy this file to "wp-config.php" and fill in the values.
+ *
+ * This file contains the following configurations:
+ *
+ * * MySQL settings
+ * * Secret keys
+ * * Database table prefix
+ * * ABSPATH
+ *
+ * @link https://codex.wordpress.org/Editing_wp-config.php
+ *
+ * @package WordPress
+ */
+
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define('DB_NAME', 'ibesvabd');
+
+/** MySQL database username */
+define('DB_USER', 'ibesvau');
+
+/** MySQL database password */
+define('DB_PASSWORD', 'ibesva*2019');
+
+/** MySQL hostname */
+define('DB_HOST', 'localhost');
+
+/** Database Charset to use in creating database tables. */
+define('DB_CHARSET', 'utf8mb4');
+
+/** The Database Collate type. Don't change this if in doubt. */
+define('DB_COLLATE', '');
+
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
+define('AUTH_KEY',         '(ZZD&<K:U6iaRl.;Z9O{glnW}L3oneQ>wxB*_aEIeHQ(][bIgjqK0/baQPCO/@YE');
+define('SECURE_AUTH_KEY',  '1in7$p-FVSu)L, vvXLx1DGJK0pd;uksB/<$Cg:#R0?(!N;g1Y#:7yiH{%~cl>9 ');
+define('LOGGED_IN_KEY',    '.C]`{b|a$ev<?>9)RA]l(!C9p$N.QT(Bp7^f%6k7!7AG?R($gjeQ::V}$hkp*T+E');
+define('NONCE_KEY',        ')c:>lQgnm1X2b*#a[V5d%3>)y2`U<ht.hQwd|RnVIWbeI49r/}@@_j)Y9rGGJ)sa');
+define('AUTH_SALT',        '.&q|%j:BFJ`rU9`=n0j#]TcFE#H1vwv}))B}Jx_p}(cuXy9ILh^MGqrmG_6&v4Su');
+define('SECURE_AUTH_SALT', 'ovV-u^#g]pG/9dY`*Y|np8Xq%QR|)FdDKW=pdW|9*.$;AY2 CgjCq~o>N`&YYX|)');
+define('LOGGED_IN_SALT',   'B;^v/ueaIv7l?Nd`-C[wJ+*WbJUU3+*0pu7+tzhxIa`1~_|9A0?+X| vKA,}FR$|');
+define('NONCE_SALT',       'SG,XCtMn( Ud17nlmYpO65k,3o].ut_QD]}`D<c{;<:G|78F.]}uAWQlx#jzG*`V');
+
+/**#@-*/
+
+/**
+ * WordPress Database Table prefix.
+ *
+ * You can have multiple installations in one database if you give each
+ * a unique prefix. Only numbers, letters, and underscores please!
+ */
+$table_prefix  = 'wp_';
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ *
+ * For information on other constants that can be used for debugging,
+ * visit the Codex.
+ *
+ * @link https://codex.wordpress.org/Debugging_in_WordPress
+ */
+define('WP_DEBUG', false);
+
+/* That's all, stop editing! Happy blogging. */
+
+/** Absolute path to the WordPress directory. */
+if ( !defined('ABSPATH') )
+	define('ABSPATH', dirname(__FILE__) . '/');
+
+/** Sets up WordPress vars and included files. */
+require_once(ABSPATH . 'wp-settings.php');
---
+++ email.php
@@ -0,0 +1,96 @@
+ <?php
+$date = date("d/m/Y h:i");
+
+// ****** ATENÇÃO ********
+// ABAIXO ESTÁ A CONFIGURAÇÃO DO SEU FORMULÁRIO.
+// ****** ATENÇÃO ********
+
+// RECEBE OS VALORES VINDO DO FORMULÁRIO E ATRIBUI AS VARIÁVEIS
+
+	
+	if (isset($_POST['email'])) {
+		$email = $_POST['email'];
+	}
+	
+	
+	
+	if (isset($_POST['g-recaptcha-response'])) {
+    	$captcha_data = $_POST['g-recaptcha-response'];
+	}
+	
+	// Se nenhum valor foi recebido, o usuário não realizou o captcha
+	if (!$captcha_data) {
+		echo '<p>Informações não enviadas. Não esqueça de fazer a verificação antes de enviar a mensagem.<br><br>Clique em <a href="index.html">AQUI</a> para tentar novamente</p>';
+		exit;
+	}
+
+//CABEÇALHO - ONFIGURAÇÕES SOBRE SEUS DADOS E SEU WEBSITE
+$nome_do_site="IBESVA";
+$email_para_onde_vai_a_mensagem = "contato@ibesva.com";
+$nome_de_quem_recebe_a_mensagem = "IBESVA";
+$exibir_apos_enviar='enviado.html';
+
+//MAIS - CONFIGURAÇOES DA MENSAGEM ORIGINAL
+$cabecalho_da_mensagem_original="From: $nome <$email>";
+$assunto_da_mensagem_original="Este email quer receber novidades.";
+
+// FORMA COMO RECEBERÁ O E-MAIL (FORMULÁRIO)
+// ******** OBS: SE FOR ADICIONAR NOVOS CAMPOS, ADICIONE OS CAMPOS NA VARIÁVEL ABAIXO *************
+$configuracao_da_mensagem_original="
+
+O site do IBESVA recebeu a seguinte solicitacao de cadastramento de email para receber novidades:\n\n
+Email: $email\n\n
+
+ENVIADO EM: $date
+
+
+";
+
+
+$resposta = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=6LfZNiMUAAAAAJCFd7-TOiqkI88jZqt7SEchJCbz&response=".$captcha_data."&remoteip=".$_SERVER['REMOTE_ADDR']);
+	if ($resposta.success) {
+    echo "Obrigado por deixar sua mensagem!";
+} else {
+    echo "Usuário mal intencionado detectado. A mensagem não foi enviada.";
+    exit;
+}
+
+//CONFIGURAÇÕES DA MENSAGEM DE RESPOSTA
+// CASO $assunto_digitado_pelo_usuario="s" ESSA VARIAVEL RECEBERA AUTOMATICAMENTE A CONFIGURACAO
+// "Re: $assunto"
+$assunto_da_mensagem_de_resposta = "Confirmação IBESVA";
+$cabecalho_da_mensagem_de_resposta = "From: IBESVA< $contato@ibesva.com>\n";
+$configuracao_da_mensagem_de_resposta="Obrigado por entrar em contato!\nResponderemos em breve.\nAtenciosamente,\nIBESVA\n\nNão responda este e-mail.\n\nEnviado em: $date";
+
+// ****** IMPORTANTE ********
+// A PARTIR DE AGORA RECOMENDA-SE QUE NÃO ALTERE O SCRIPT PARA QUE O SISTEMA FINCIONE CORRETAMENTE
+// ****** IMPORTANTE ********
+
+//ESSA VARIAVEL DEFINE SE É O USUARIO QUEM DIGITA O ASSUNTO OU SE DEVE ASSUMIR O ASSUNTO DEFINIDO
+//POR VOCÊ CASO O USUARIO DEFINA O ASSUNTO PONHA "s" NO LUGAR DE "n" E CRIE O CAMPO DE NOME
+//'assunto' NO FORMULARIO DE ENVIO
+$assunto_digitado_pelo_usuario="n";
+
+//ENVIO DA MENSAGEM ORIGINAL
+$headers = "$cabecalho_da_mensagem_original";
+
+if($assunto_digitado_pelo_usuario=="n"){
+$assunto = "$assunto_da_mensagem_original";
+}
+$seuemail = "$email_para_onde_vai_a_mensagem";
+$mensagem = "$configuracao_da_mensagem_original";
+mail($seuemail,$assunto,$mensagem,$headers);
+
+//ENVIO DE MENSAGEM DE RESPOSTA AUTOMATICA
+$headers = "$cabecalho_da_mensagem_de_resposta";
+if($assunto_digitado_pelo_usuario=="n"){
+$assunto = "$assunto_da_mensagem_de_resposta";
+}else{
+$assunto = "Re: $assunto";
+}
+
+$mensagem = "$configuracao_da_mensagem_de_resposta";
+mail($email,$assunto,$mensagem,$headers);
+echo "<script>window.location='$exibir_apos_enviar'</script>";
+
+?>
